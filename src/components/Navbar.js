98a627772455be90fcb02f30703c995faf84076c
mohitCodepy <mohit1511199@gmail.com>
2022-03-25 10:56:33 +0530

    navbar padding correction

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,7 +14,7 @@ export default function Navbar() {
   const [NavItems, setNavItems] = useState([{ 'name': 'Explore' },
   { 'name': 'Problems' },
   { 'name': 'Contest' },
-  { 'name': 'Disussion' },
+  { 'name': 'Discuss' },
   { 'name': 'Interview' }])
 
 
@@ -27,7 +27,7 @@ export default function Navbar() {
             <img  alt='leetcode logo' src="https://leetcode.com/_next/static/images/logo-dark-c96c407d175e36c81e236fcfdd682a0b.png" width={18} height={23} />
             <Box style={{ display: 'flex', alignItems: 'center', textAlign: 'center', margin: '0px 25px'  }}>
               {NavItems.map((it) => {
-                return <Typography variant='sm' style={{ margin: '10px', padding:'14px 0px', color:currentNav === it.name?'white':'rgba(239,241,246,.75)', fontWeight:currentNav === it.name?'900px':'100px', borderBottom:currentNav === it.name?'2px solid white':'none', borderRadius:'2px', borderBottomWidth:3}}>{it.name}</Typography>
+                return <Typography variant='xs' style={{ margin: '10px', padding:'14px 0px', color:currentNav === it.name?'rgba(255,255,255,var(1));':'rgba(239,241,246,.75)', fontWeight:currentNav === it.name?'1000px':'100px', borderBottom:currentNav === it.name?'2px solid white':'none', borderRadius:'2px', borderBottomWidth:3}}>{it.name}</Typography>
               })}
             </Box>
           </Toolbar>
