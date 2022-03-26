e87a14c79aa4d9c93b2e16270e8f951d68e34f5e
mohitCodepy <mohit1511199@gmail.com>
2022-03-26 19:47:39 +0530

    body color correction

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,12 +15,14 @@ import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined
 export default function Navbar() {
 
   const currentNav = 'Problems'
-  const NavItems = [{ 'name': 'Explore' },
-  { 'name': 'Problems' },
-  { 'name': 'Contest' },
-  { 'name': 'Discuss' },
-  { 'name': 'Interview', type: 'dropdown' },
-  { 'name': 'Store', type: 'dropdown', color: 'orange' }];
+  const NavItems = [
+    { 'name': 'Explore' },
+    { 'name': 'Problems' },
+    { 'name': 'Contest' },
+    { 'name': 'Discuss' },
+    { 'name': 'Interview', type: 'dropdown' },
+    { 'name': 'Store', type: 'dropdown', color: 'orange' }
+  ];
 
 
   const NavStyle = (it) => {
@@ -48,11 +50,11 @@ export default function Navbar() {
                 return (<><Typography style={NavStyle(it)} key={it.name}>{it.name}</Typography>{it.type ? <ArrowDropDownOutlinedIcon /> : ''}</>)
               })}
             </Box>
-            <Box style={{ display: 'flex', alignItems: 'center', justifyContent: 'end', marginRight:'20px', textAlign: 'center', width: "100%" }}>
-              <Button size='sm' variant="contained" style={{ textTransform: 'capitalize', backgroundColor: 'hsla(0,0%,100%,.1)', color: 'orange' }}>Premium</Button>
-              <Button style={{ 'marginLeft': '15px', color:'#8B8A8B' }}><QueuePlayNextOutlinedIcon /></Button>
-              <Button style={{ 'marginRight': '10px', color:'#8B8A8B' }}><NotificationsOutlinedIcon /></Button>
-              <Avatar alt="mohit rathore" src="https://assets.leetcode.com/users/mohitOnlyCodeLover/avatar_1621591285.png" style={{height: 23, width:23}}/>
+            <Box style={{ display: 'flex', alignItems: 'center', justifyContent: 'end', marginRight: '50px', textAlign: 'center', width: "100%" }}>
+              <Button size='small' variant="contained" style={{ textTransform: 'capitalize', backgroundColor: 'hsla(0,0%,100%,.1)', color: 'orange' }}>Premium</Button>
+              <Button style={{ 'marginLeft': '13px', color: '#8B8A8B' }}><QueuePlayNextOutlinedIcon /></Button>
+              <Button style={{ 'marginRight': '5px', color: '#8B8A8B' }}><NotificationsOutlinedIcon /></Button>
+              <Avatar alt="mohit rathore" src="https://assets.leetcode.com/users/mohitOnlyCodeLover/avatar_1621591285.png" style={{ height: 23, width: 23 }} />
             </Box>
           </Toolbar>
         </Container>
