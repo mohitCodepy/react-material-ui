60224911b8dd732a45a2c141abaac819c570bd87
mohitCodepy <mohit1511199@gmail.com>
2022-04-04 00:04:53 +0530

    drawer bug resolved

--- src/components/Drawer.js
+++ src/components/Drawer.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Container, Box, Typography, Chip } from '@material-ui/core'
 import drawerData from '../Jsondata/drawerData.json'
 
@@ -8,28 +8,29 @@ export default function Drawer() {
     const [overFlow, setoverFlow] = useState('auto');
     const changeHeight = () => {
         console.log('clicked')
-        setdivHeight('5vh');
-        setoverFlow('hide');
+        divHeight === '80vh' ? setdivHeight('9vh') : setdivHeight('80vh') ;
+        overFlow === 'auto'? setoverFlow('hidden'): setoverFlow('auto')
     }
+    
 
     return (
         <div>
             <Container style={{ marginTop: '40px' }}>
-                <Container style={{ justifyContent: 'space-between', color: 'white', margin: '20px auto', width: '100%', border: '2px solid red' }}>
-                    <Box sx={{ color: 'white', lineHeight: '80px', height: { divHeight }, overflow: { overFlow } }}>
-                    {drawerData['data'].map((dt)=>{
-                        return (
-                            <a href='/' key={dt.name} style={{'margin': '15px', 'color': 'white', 'textDecoration': 'none'}}>
-                                <Typography style={{'display': 'inline', 'marginRight': '5px'}} variant="body2">{dt.name}</Typography>
-                                <Chip label={dt.num} size="small"/>
-                            </a>
-                        )
-                    })}
+                <Box>
+                    <Box sx={{ color: 'white',  margin: '20px auto', width: '100%', border: '2px solid red', color: 'white', 'textAlign': 'left', lineHeight: '80px', height: divHeight , overflow:  overFlow }}>
+                        {drawerData['data'].map((dt) => {
+                            return (
+                                <a href='/' key={dt.name} style={{ 'margin': '5px', 'color': 'white', 'textDecoration': 'none' }}>
+                                    <Typography style={{ 'display': 'inline', 'marginRight': '5px' }} variant="body2">{dt.name}</Typography>
+                                    <Chip label={dt.num} size="small" />
+                                </a>
+                            )
+                        })}
                     </Box>
                     <Box sx={{ width: '100%' }} onClick={changeHeight}>
                         hide
                     </Box>
-                </Container>
+                </Box>
             </Container>
         </div>
     )
