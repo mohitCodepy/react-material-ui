a65bb7f20ffa32ec5365ef249ccf2a9b70ac20c4
mohitCodepy <mohit1511199@gmail.com>
2022-04-03 23:41:32 +0530

    added chips and tag

--- src/components/Drawer.js
+++ src/components/Drawer.js
@@ -1,12 +1,36 @@
-import React from 'react'
-import { Container } from '@material-ui/core'
+import React, { useState } from 'react'
+import { Container, Box, Typography, Chip } from '@material-ui/core'
+import drawerData from '../Jsondata/drawerData.json'
 
 export default function Drawer() {
-  return (
-    <div>
-      <Container>
-            
-      </Container>
-    </div>
-  )
+
+    const [divHeight, setdivHeight] = useState('80vh');
+    const [overFlow, setoverFlow] = useState('auto');
+    const changeHeight = () => {
+        console.log('clicked')
+        setdivHeight('5vh');
+        setoverFlow('hide');
+    }
+
+    return (
+        <div>
+            <Container style={{ marginTop: '40px' }}>
+                <Container style={{ justifyContent: 'space-between', color: 'white', margin: '20px auto', width: '100%', border: '2px solid red' }}>
+                    <Box sx={{ color: 'white', lineHeight: '80px', height: { divHeight }, overflow: { overFlow } }}>
+                    {drawerData['data'].map((dt)=>{
+                        return (
+                            <a href='/' key={dt.name} style={{'margin': '15px', 'color': 'white', 'textDecoration': 'none'}}>
+                                <Typography style={{'display': 'inline', 'marginRight': '5px'}} variant="body2">{dt.name}</Typography>
+                                <Chip label={dt.num} size="small"/>
+                            </a>
+                        )
+                    })}
+                    </Box>
+                    <Box sx={{ width: '100%' }} onClick={changeHeight}>
+                        hide
+                    </Box>
+                </Container>
+            </Container>
+        </div>
+    )
 }
