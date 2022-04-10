6c6ecfcc3d6c58f455e760b5973df5826e2a05ec
mohitCodepy <mohit1511199@gmail.com>
2022-04-10 23:55:29 +0530

    created chips and styles

--- src/components/CategoryChips.js
+++ src/components/CategoryChips.js
@@ -1,10 +1,42 @@
 import React from 'react'
+import { Container, Chip, Box } from '@mui/material';
+import SchemaOutlinedIcon from '@mui/icons-material/SchemaOutlined';
+import Inventory2OutlinedIcon from '@mui/icons-material/Inventory2Outlined';
+import SdStorageOutlinedIcon from '@mui/icons-material/SdStorageOutlined';
+import RouteOutlinedIcon from '@mui/icons-material/RouteOutlined';
+import PriceCheckOutlinedIcon from '@mui/icons-material/PriceCheckOutlined';
+
+
+const chipData = [
+  { name: 'All Topics', 'image': <Inventory2OutlinedIcon fontSize="small" />, 'active': 'true' },
+  { name: 'Algorithms', 'image': <SchemaOutlinedIcon fontSize="small" style={{ color: "orange" }} /> },
+  { name: 'Database', 'image': <SdStorageOutlinedIcon fontSize="small" style={{ color: "blue" }} /> },
+  { name: 'Shell', 'image': <PriceCheckOutlinedIcon fontSize="small" style={{ color: "green" }} /> },
+  { name: 'Concurrency', 'image': <RouteOutlinedIcon fontSize="small" style={{ color: "purple" }} /> },
+
+]
+
+const chipStyle = (ch) => {
+  return {
+    marginLeft: '5px',
+    marginRight: '5px',
+    color: ch.active === 'true' ? 'black': 'white',
+    backgroundColor: ch.active === 'true' ? 'whitesmoke' : 'hsla(0,0%,100%,.1)',
+    width: '130px',
+    height: '40px',
+    borderRadius: '26px',
+    border: 'transparent'
+  }
+}
 
 export default function CategoryChips() {
   return (
     <Box>
-      <Container >
-
+      <Container style={{ justifyContent: 'flex-start', display: 'flex' }}>
+        {chipData.map((ch) => {
+          return (<Chip sx={chipStyle(ch)} icon={ch.image} label={ch.name} size='big' variant="outlined" />)
+        })
+        }
       </Container>
     </Box>
   )
--- src/components/Main.js
+++ src/components/Main.js
@@ -4,19 +4,21 @@ import Contest from './Contest'
 import SideBar from './SideBar'
 import StudyPlan from './StudyPlan'
 import Drawer from './Drawer'
+import CategoryChips from './CategoryChips'
 
 export default function Main() {
     return (
         <div style={{ marginTop: '25px', display: 'block'}}>
             <Container>
                 <Grid container>
-                    <Grid item lg={9}>
+                    <Grid item lg={9} style={{'marginBottom': '100px'}}>
                         <Contest />
                         <StudyPlan />
                         <Container>
                             <Divider style={{ marginTop: '80px', width: '100%', backgroundColor: 'rgba(255,255,255,15%)' }} />
                         </Container>
                         <Drawer />
+                        <CategoryChips />
                     </Grid>
                     <Grid item lg={3}>
                         <SideBar />
